af9943dc96f931ebf031a75e1bcef58fc2314885
Danila Belov <danilabelov.adm@yandex.ru>
2023-10-20 18:16:50 +0300

    Achieved 100% test coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,6 @@
-from pytest_proj.utils import arrs
+import pytest
+
+from utils import arrs
 
 
 def test_get():
@@ -9,3 +11,7 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 3, 4, 5], 0) == [1, 3, 4, 5]
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([1, 3, 4, 5, 6], None, 2) == [1, 3]
